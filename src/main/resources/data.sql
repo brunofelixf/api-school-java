fe8f16e4be5fbff66473e4331a06586acf82b3be
brunofelixf <brunofelixf@gmail.com>
2023-08-17 16:23:53 -0300

    feat: add course entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://static.vecteezy.com/system/resources/previews/007/559/429/original/wolf-a-wolf-logo-illustration-in-modern-style-free-vector.jpg','https://clipart-library.com/images/gceookAdi.jpg');
